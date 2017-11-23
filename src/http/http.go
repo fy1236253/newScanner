1ce288e2fcf0865d3e1463464169dde126dc3049
fengya <zhangfengya@yunlaba.com>
2017-11-23 17:06:54 +0800

    new

--- src/http/http.go
+++ src/http/http.go
@@ -49,7 +49,7 @@ func init() {
 func Start() {
 	// 静态资源请求
 	ConfigWebHTTP()
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/v1", func(w http.ResponseWriter, r *http.Request) {
 		http.FileServer(http.Dir(filepath.Join(g.Root, "/public"))).ServeHTTP(w, r)
 	})
 	// start http server
